d96b948389d825e3cc168be2274066a1c098afea
Gustavo Lepri <gustavolepri@gmail.com>
2015-03-03 17:16:51 -0300

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0a4'
+version = '1.0a5.dev0'
 
 long_description = (
     read('README.rst')
